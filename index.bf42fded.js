ecae2413c612fdd37f002fbd65deb4a4ee8912f2
Code Sandbox <hello@codesandbox.io>
2023-09-20 08:32:26 +0000

    Deployment from Codesandbox.

---
+++ index.bf42fded.js
@@ -0,0 +1,3 @@
+const e=document.getElementById("blueButton"),t=document.getElementById("redButton"),n=document.getElementById("para");e.addEventListener("click",function(){n.style.color="blue"}),t.addEventListener("click",function(){n.style.color="red"}//# sourceMappingURL=index.bf42fded.js.map
+);
+//# sourceMappingURL=index.bf42fded.js.map
